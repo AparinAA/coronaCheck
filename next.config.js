5b13e2bff8a95b595362475ea5da1a76c80b14be
AparinAA <alxaparin@gmail.com>
2022-10-06 20:59:18 +0300

    fixed

--- next.config.js
+++ next.config.js
@@ -21,6 +21,13 @@ const nextConfig = {
     unoptimized: true,
   },
   assetPrefix,
+  env: {
+    AUTH0_BASE_URL: process.env.AUTH0_BASE_URL || 'http://localhost:3000',
+    AUTH0_SECRET: process.env.AUTH0_SECRET,
+    AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL || 'https://dev-m8xtdhux.us.auth0.com',
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET
+  }
 }
 
 module.exports = nextConfig;
