209007942f158bbf3ca76394731702028d470432
AparinAA <alxaparin@gmail.com>
2022-10-06 15:57:13 +0300

    fixed

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     domains: ['reqres.in', 's.gravatar.com'],
     formats: ['image/avif', 'image/webp'],
   },
