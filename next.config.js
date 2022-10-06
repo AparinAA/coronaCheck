bb30a990b40e08b70571b471f09fc475c3209f9b
AparinAA <alxaparin@gmail.com>
2022-10-06 16:18:55 +0300

    fixed

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    domains: ['reqres.in', 's.gravatar.com'],
-    formats: ['image/avif', 'image/webp'],
-  },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  
+}
+const images = {
+  dangerouslyAllowSVG: true,
+  contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
 }
 
+module.exports = { 
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  nextConfig,
+  images
+}
 
-module.exports = nextConfig;
+//domains: ['reqres.in', 's.gravatar.com'],
+//formats: ['image/avif', 'image/webp'],
