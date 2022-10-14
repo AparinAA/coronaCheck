8d03044db0bf67bcf85b524db7a9e5c6f5902442
AparinAA <alxaparin@gmail.com>
2022-10-14 17:07:21 +0300

    add db

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ import axios from 'axios';
 import {useEffect} from 'react';
 
 const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
-console.info(process.env.GITHUB_ENV, process.env.NEXT_PABLIC_HOST);
+console.info(process.env.GITHUB_ENV, process.env.NEXT_PUBLIC_BASE_PATH);
 
 const Home: NextPage = () => {
     const { user, error, isLoading } = useUser();
