c94f01e99660496a2e615bbcb47a7817d48e84ed
AparinAA <alxaparin@gmail.com>
2022-10-14 16:09:59 +0300

    add db

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ import axios from 'axios';
 import {useEffect} from 'react';
 
 const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
-console.info(process.env.NEXT_PUBLIC_BASE_PATH );
+console.info(process.env.NEXT_PUBLIC_BASE_PATH, process.env.NEXT_PUBLIC_MYSQL_PORT );
 
 const Home: NextPage = () => {
     const { user, error, isLoading } = useUser();
