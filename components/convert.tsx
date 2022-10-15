96492c321d6b1b6ef42fbd47678a4c8c75b0fca8
AparinAA <alxaparin@gmail.com>
2022-10-15 13:38:16 +0300

    add db

--- components/convert.tsx
+++ components/convert.tsx
@@ -49,7 +49,7 @@ class Convert extends React.Component<IProps, IState, Value> {
     }
     
     componentDidMount(): void {
-        axios.get<currenciesRate>(`${process.env.NEXT_PUBLIC_BASE_PATH ?? ''}/api/checkRate`)
+        axios.get<currenciesRate>(`api/checkRate`)
         .then(res => this.setState(res?.data))
         .catch( (error: any) => console.info(error));
     }
