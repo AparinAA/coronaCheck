b778ffb8b733a09ada8094f75bcc371bf460a3a2
AparinAA <alxaparin@gmail.com>
2022-10-15 14:00:21 +0300

    add db

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,7 @@ import { UserProvider } from '@auth0/nextjs-auth0'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider>
       <Component {...pageProps} />
-    </UserProvider>
   )
 }
 
